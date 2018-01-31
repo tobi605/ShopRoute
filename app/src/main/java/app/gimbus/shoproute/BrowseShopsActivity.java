1305389e03dcef70de9ad8d1759ab27dbbc45daf
Tobiasz <tobi605@gmail.com>
2018-01-31 15:49:19 +0100

    added Shop class added array adapter

--- app/src/main/java/app/gimbus/shoproute/BrowseShopsActivity.java
+++ app/src/main/java/app/gimbus/shoproute/BrowseShopsActivity.java
@@ -2,7 +2,10 @@ package app.gimbus.shoproute;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by tobi6 on 21.01.2018.
@@ -13,5 +16,10 @@ public class BrowseShopsActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedinstance){
         super.onCreate(savedinstance);
         setContentView(R.layout.shop_browse);
+        ListView shopBrowseList = findViewById(R.id.shop_browse_list);
+        //TODO from file to array of shops
+        Shop[] tempArray = {new Shop("Aldi"), new Shop("Lidl")};
+        ArrayAdapter<Shop> shopArrayAdapter = new ArrayAdapter<>(this, R.layout.shop_browse_item, tempArray);
+        shopBrowseList.setAdapter(shopArrayAdapter);
     }
 }
---
+++ app/src/main/java/app/gimbus/shoproute/Shop.java
@@ -0,0 +1,13 @@
+package app.gimbus.shoproute;
+
+/**
+ * Created by tobi6 on 31.01.2018.
+ */
+
+class Shop {
+    private String name;
+
+    public Shop(String name) {
+        this.name = name;
+    }
+}
