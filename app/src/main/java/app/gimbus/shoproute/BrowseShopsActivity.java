9ada64a8198f4d958ccc715f806670c4c4ee54cd
Tobiasz <tobi605@gmail.com>
2018-01-31 14:18:51 +0100

    nothing

--- app/src/main/java/app/gimbus/shoproute/BrowseShopsActivity.java
+++ app/src/main/java/app/gimbus/shoproute/BrowseShopsActivity.java
@@ -2,6 +2,7 @@ package app.gimbus.shoproute;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 /**
  * Created by tobi6 on 21.01.2018.
